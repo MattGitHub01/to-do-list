4de78d94199d52305359ba5fb99b1da08d9f6906
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-23 13:54:25 -0600

    Run webpack, install html webpack plugin, css plugins, create webpack config file, package.json,  index html/js/css files, webpack config set for custom fonts, images and css.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+function index () {
+    const container = document.createElement('div');
+
+    return container
+}
+
+document.body.appendChild(index());
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resources',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-Do',
+            filename: 'index.html',
+            inject: 'head',
+            scriptLoading: 'defer'
+        })
+    ]
+};
