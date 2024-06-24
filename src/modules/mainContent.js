f30a02baa468f3e6d6219f9500c1c1dbed85c299
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-24 14:08:54 -0600

    Fix a few bugs in form, going to put form in it's own module

--- src/modules/mainContent.js
+++ src/modules/mainContent.js
@@ -1,6 +1,6 @@
 export function mainContent() {
     const modalProjectForm = document.createElement('dialog');
-    modalProjectForm.classList.add('f-new-project-form');
+    modalProjectForm.classList.add('modal-new-project-form');
     document.body.append(modalProjectForm);
 
     const formTag = document.createElement('form');
@@ -10,32 +10,36 @@ export function mainContent() {
     modalProjectForm.appendChild(formTag);
 
     const exitDiv = document.createElement('div');
-    exitDiv.classList.add('f-exit-btn-div');
+    exitDiv.classList.add('modal-exit-btn-div');
     modalProjectForm.appendChild(exitDiv);
 
     const formExit = document.createElement('button');
-    formExit.classList.add('f-form-exit-btn');
+    formExit.classList.add('modal-form-exit-btn');
     formExit.setAttribute('formnovalidate', '');
+    formExit.setAttribute('data-bs-dismiss', 'modal');
     formExit.textContent = 'X';
+    formExit.addEventListener('click', () => {
+        modalProjectForm.close();
+    });
     exitDiv.appendChild(formExit);
 
     const formUl = document.createElement('ul');
-    formUl.classList.add('f-form-ul');
+    formUl.classList.add('modal-form-ul');
     modalProjectForm.appendChild(formUl);
     
     // Title Form Input
     const titleLi = document.createElement('li');
-    titleLi.classList.add('f-title-li');
+    titleLi.classList.add('modal-title-li');
     formUl.appendChild(titleLi);
 
     const titleLabel = document.createElement('label');
     titleLabel.textContent = `Title: `;
-    titleLabel.classList.add('f-label');
+    titleLabel.classList.add('modal-label');
     titleLabel.setAttribute('for', 'title');
     titleLi.appendChild(titleLabel);
 
     const titleInput = document.createElement('input');
-    titleInput.classList.add('f-input');
+    titleInput.classList.add('modal-input');
     titleInput.setAttribute('type', 'text');
     titleInput.setAttribute('name', 'title');
     titleInput.setAttribute('id', 'title');
@@ -44,17 +48,17 @@ export function mainContent() {
 
     // Date Form Input
     const dateLi = document.createElement('li');
-    dateLi.classList.add('f-date-li');
+    dateLi.classList.add('modal-date-li');
     formUl.appendChild(dateLi);
     
     const dateLabel = document.createElement('label');
     dateLabel.textContent = `Date: `;
-    dateLabel.classList.add('f-label');
+    dateLabel.classList.add('modal-label');
     dateLabel.setAttribute('for', 'date');
     dateLi.appendChild(dateLabel);
 
     const dateInput = document.createElement('input');
-    dateInput.classList.add('f-input');
+    dateInput.classList.add('modal-input');
     dateInput.setAttribute('type', 'date');
     dateInput.setAttribute('name', 'date');
     dateInput.setAttribute('id', 'date');
@@ -63,57 +67,61 @@ export function mainContent() {
 
     // Priority Form Input
     const priorityLi = document.createElement('li');
-    priorityLi.classList.add('f-priority-li');
+    priorityLi.classList.add('modal-priority-li');
     formUl.appendChild(priorityLi);
 
     const priorityLabel = document.createElement('label');
     priorityLabel.textContent = `Priority: `;
-    priorityLabel.classList.add('f-label');
+    priorityLabel.classList.add('modal-label');
     priorityLabel.setAttribute('for', 'priority');
     priorityLi.appendChild(priorityLabel);
 
     const priorityInput = document.createElement('select');
-    priorityInput.classList.add('f-input');
+    priorityInput.classList.add('modal-input');
     priorityInput.setAttribute('name', 'priority');
     priorityInput.setAttribute('id', 'priority'); 
     priorityLi.appendChild(priorityInput);
 
     const highPriority = document.createElement('option');
+    highPriority.setAttribute('id', 'priority'); 
     highPriority.textContent = `High`;
     highPriority.setAttribute('value', 'high');
     priorityInput.appendChild(highPriority);
 
     const mediumPriority = document.createElement('option');
+    mediumPriority.setAttribute('id', 'priority'); 
     mediumPriority.textContent = `Medium`;
     mediumPriority.setAttribute('value', 'medium');
     priorityInput.appendChild(mediumPriority);
 
     const lowPriority = document.createElement('option');
+    lowPriority.setAttribute('id', 'priority'); 
     lowPriority.textContent = `Low`;
     lowPriority.setAttribute('value', 'high');
     priorityInput.appendChild(lowPriority);
 
     // Description Form Input
     const descriptionLi = document.createElement('li');
-    descriptionLi.classList.add('f-description-li');
+    descriptionLi.classList.add('modal-description-li');
     formUl.appendChild(descriptionLi);
 
     const descriptionLabel = document.createElement('label');
     descriptionLabel.textContent = `Description: `;
-    descriptionLabel.classList.add('f-description-label');
+    descriptionLabel.classList.add('modal-description-label');
     descriptionLabel.setAttribute('for', 'description');
     descriptionLi.appendChild(descriptionLabel);
 
     const descriptionInput = document.createElement('textarea');
-    descriptionInput.classList.add('f-description-input');
-    priorityInput.setAttribute('name', 'description');
-    priorityInput.setAttribute('id', 'description');
-    priorityInput.setAttribute('rows', '4');
-    priorityInput.setAttribute('cols', '50');
-    priorityInput.setAttribute('required', '');
+    descriptionInput.classList.add('modal-description-input');
+    descriptionInput.setAttribute('name', 'description');
+    descriptionInput.setAttribute('id', 'description');
+    descriptionInput.setAttribute('rows', '4');
+    descriptionInput.setAttribute('cols', '50');
+    descriptionInput.setAttribute('required', '');
     descriptionLi.appendChild(descriptionInput);
-
     // Creates modal form for project info submission
+
+
 
     const main = document.createElement('main');
     main.classList.add('main');
